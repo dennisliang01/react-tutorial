29c1904aca72ef912f3a9738319547aca34896a3
Dennis Liang <Dennisliang01@gmail.com>
2024-06-20 10:27:53 -0700

    lesson 18

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,26 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     // useEffect runs every time the component renders
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {  // then fires once the fetch is complete
                 return res.json();
             })
             .then(data => {
-                console.log(data);
                 setBlogs(data); // setBlogs is a function to update the state
-            })
+                setIsPending(false);
+            });
+        }, 1000);
+       
     }, []) // the empty array means it only runs on the first render
 
   return (
     <div className="home">
+        {isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
