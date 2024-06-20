6a5469c9ab9777e884cdffddc93921258e4594b4
Dennis Liang <Dennisliang01@gmail.com>
2024-06-20 10:00:07 -0700

    lesson 17

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,23 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-
-    const [name, setName] = useState('mario');
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     // useEffect runs every time the component renders
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
-    }, [name]) // the empty array means it only runs on the first render
+        fetch('http://localhost:8000/blogs')
+            .then(res => {  // then fires once the fetch is complete
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data); // setBlogs is a function to update the state
+            })
+    }, []) // the empty array means it only runs on the first render
 
   return (
     <div className="home">
-        <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-        <button onClick={() => setName('luigi')}>change name</button>
-        <p>{ name }</p> 
+        {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
 }
