b9c7073a98a7cb57812bfaee1ae34759532d69cb
Dennis Liang <Dennisliang01@gmail.com>
2024-06-20 09:25:14 -0700

    lesson 13

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Home from './Home';
 
 // JSX Template
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com"
 
   return (
     <div className="App">
