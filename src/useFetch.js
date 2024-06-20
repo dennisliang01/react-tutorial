52e81f259556a2c060f8bd71d8b1a5c6eda639b1
Dennis Liang <Dennisliang01@gmail.com>
2024-06-20 11:51:00 -0700

    lesson 24
    cleanup function

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,11 @@ const useFetch = (url) => { // custom hook function needs to start with "use"
     const [error, setError] = useState(null);
 
     useEffect(() => {
+
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, {signal: abortCont.signal})
                 .then(res => {  // then fires once the fetch is complete
                     if (!res.ok) {
                         throw Error('Could not fetch the data for that resource');
@@ -20,11 +23,18 @@ const useFetch = (url) => { // custom hook function needs to start with "use"
                     setError(null);
                 })
                 .catch(err => {
-                    console.log(err.message);
-                    setError(err.message);
-                    setIsPending(false);
+                    if (err.name == 'AbortError') {
+                        console.log('fetch aborted');
+                    } else {
+                        console.log(err.message);
+                        setError(err.message);
+                        setIsPending(false);
+                    }
+                   
                 })
         }, 1000);
+
+        return () => abortCont.abort();
     }, [url]);
 
     return { data, isPending, error };
